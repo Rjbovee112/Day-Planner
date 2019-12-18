7defb2d7816194d90d562bc5870d3acec9b67bdc
Robert Bovee <Rjbovee112@gmail.com>
2019-12-17 18:43:26 -0700

    updated clock with own page, local stoarge and color changing time

---
+++ clock.js
@@ -0,0 +1,9 @@
+//Clock//
+const clock = document.getElementById('clock');
+
+function updateTime() {
+    const now = moment();
+    const humanReadable = now.format('MMMM Do YYYY, h:mm a');
+
+    clock.textContent = humanReadable;
+}
--- code.js
+++ code.js
@@ -1,28 +1,57 @@
 
 
-//Clock//
-const clock = document.getElementById('clock');
-
-function updateTime() {
-    const now = moment();
-    const humanReadable = now.format('MMMM Do YYYY, h:mm a');
-
-    clock.textContent = humanReadable;
-}
-
 setInterval(updateTime, 1000);
 updateTime();
 
-//java local storage
+//java local storage?
 
 function save() {
     var saveApt = document.getElementById('wtime').value;
-    localStorage.setItem('text', wtime);
+    localStorage.setItem('textarea', wtime);
 }
 
 function load() {
-    var storedValue = localStorage.getItem('text');
+    var storedValue = localStorage.getItem('textarea');
     if (storedValue) {
         document.getElementById('wtime').value = storedValue;
     }
 }
+
+
+//Riley help 
+
+$(document).ready(funtion(){
+
+    $(".btn btn-primary").on("click", function () {
+        console.log($(this))
+    })
+
+    function hoursUpdater() {
+    var currentHour = moment().hours();
+    console.log(currentHour);
+
+    $(".wtime").each(function) {
+
+    var blockHour = ($(this).attr("id").split("-")[1])
+        console.log(typeof blockHour);
+    console.log(typeof currentHour);
+
+    if(blockHour <currentHour) {
+
+        console.log("Block Hour is before current hour");
+
+    }else if(condition) {
+        if 
+    }
+}
+}
+
+    console.log("hoursUpdater funtion did run");
+};
+
+console.log("hoursUpdater is ready");
+hoursUpdater();
+
+var setInterval = setInterval(hoursUpdater, 15000)
+
+})
