1dc504727fa8c329227a8e4fc3bd4d9f99167384
Robert Bovee <Rjbovee112@gmail.com>
2019-12-19 18:41:03 -0700

    1/2 local storage

--- code.js
+++ code.js
@@ -2,34 +2,45 @@
 
 //java local storage?
 
-$(".btn btn-primary").on("click", function () {
-    var comment0 = $.trim($("#act-9").val());
-    if (comment0 != '') {
-        alert(comment0);
-        localStorage.setItem('task-0', comment0)
+
+$('.btn').on('click', function () {
+    var key = $(this).siblings()[0];
+    key = $(key).attr('id')
+    // console.log(key)
+    var comment = $("#" + key).val()
+    localStorage.setItem(key, comment)
+})
+
+function printTasks() {
+
+
+}
+
+
+//Riley help/BTB help - jquery for colors changing time 
+
+
+function hourColor() {
+    var currentHour = moment().hours()
+    // console.log(currentHour);
+    $(".wtime").each(function () {
+        var blockHour = parseInt($(this).attr("id").split("-")[1]);
+        console.log(blockHour);
+        if (blockHour < currentHour) {
+            console.log('Hour is past')
+            $(this).addClass("past");
+        } else if (blockHour > currentHour) {
+            console.log('Hour is soon')
+            $(this).addClass("future");
+        } else {
+            console.log('Hour is now')
+            $(this).addClass("present");
+        }
+    })
+    if (currentHour <= 8 || currentHour >= 18) {
+        clearStorage();
     }
 
-
-    //Riley help/BTB help - jquery for colors changing time 
-
-
-    function hourColor() {
-        var currentHour = moment().hours()
-        // console.log(currentHour);
-        $("wtime").each(function () {
-            var blockHour = parseInt($(this).attr("id").split("-")[1]);
-            console.log(blockHour);
-            if (blockHour < currentHour) {
-                console.log('Hour is past')
-                $(this).addClass("past");
-            } else if (blockHour > currentHour) {
-                console.log('Hour is soon')
-                $(this).addClass("future");
-            } else {
-                console.log('Hour is now')
-                $(this).addClass("present");
-            }
-        })
-        console.log("Dont touch this function it works")
-    }
+    console.log("Dont touch this function it works")
+}
 
